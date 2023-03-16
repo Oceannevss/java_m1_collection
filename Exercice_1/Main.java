3a95448674f98e68ca51eaca4cbb26d99acc3591
Oceannevss <pandoresaoceanne@outlook.fr>
2023-03-16 15:02:38 +0100

    modify exo 1

--- Exercice_1/Main.java
+++ Exercice_1/Main.java
@@ -1,15 +1,16 @@
 package Exercice_1;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
 
         //Exercice 1 : inverser une chaine de caractere
         var mot = "abcd";
-        var list = new LinkedList<Character>();
+        var list = new Stack<Character>();
         for (int i=mot.length()-1; i >= 0 ; i--){
-            list.addLast(mot.charAt(i));
+            list.push(mot.charAt(i));
         }
         System.out.println(list);
         var nouveauMot = "";
