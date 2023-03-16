f0d700ccd695c034d45e689d250b9a4c5855333b
Oceannevss <pandoresaoceanne@outlook.fr>
2023-03-16 15:06:22 +0100

    modify exo 2

--- Exercice_2/Main.java
+++ Exercice_2/Main.java
@@ -3,25 +3,25 @@ package Exercice_2;
 import java.util.Stack;
 
 public class Main {
-    public static boolean estVerifie(String mot) {
+    public static String estVerifie(String mot) {
         var stack = new Stack<Character>();
         for (int i = 0; i < mot.length(); i++) {
             if (mot.charAt(i) == '(' || mot.charAt(i) == '<' || mot.charAt(i) == '[') {
                 stack.push(mot.charAt(i));
             } else if (mot.charAt(i) == ')' || mot.charAt(i) == '>' || mot.charAt(i) == ']') {
                 if (stack.isEmpty()) {
-                    return false;
+                    return "Non vérifié";
                 } else {
                     var lastCaracter = stack.pop();
                     if ((mot.charAt(i) == ')' && lastCaracter != '(')
                             || (mot.charAt(i) == '>' && lastCaracter != '<')
                             || (mot.charAt(i) == ']' && lastCaracter != '[')) {
-                        return false;
+                        return "Non vérifié";
                     }
                 }
             }
         }
-        return true;
+        return "Vérifié";
     }
     public static void main(String[] args) {
 
